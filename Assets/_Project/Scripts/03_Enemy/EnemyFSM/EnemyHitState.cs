9d352c356a7e6a0aea05dc9a4729e94967f03cf0
Seungwan Son <161847275+zouea4879@users.noreply.github.com>
2026-04-02 21:56:03 +0900

    Add Enemy Attack Hit

--- Assets/_Project/Scripts/03_Enemy/EnemyFSM/EnemyHitState.cs
+++ Assets/_Project/Scripts/03_Enemy/EnemyFSM/EnemyHitState.cs
@@ -2,24 +2,51 @@ using UnityEngine;
 
 public class EnemyHitState : EnemyState
 {
+    private EnemyAnimationEventHandler animationEventHandler;
+
 
     public EnemyHitState(EnemyAI enemy, EnemyStateMachine stateMachine)
         : base(enemy, stateMachine) { }
 
     public override void Enter()
     {
+        animationEventHandler = enemy.AnimationEventHandler;
+        animationEventHandler.OnHitEnd += HandleHitEnd;
+
         enemy.Agent.isStopped = true;
         enemy.Agent.updateRotation = false;
+
+        enemy.Animator.SetTrigger("Hit");
     }
 
     public override void Exit()
     {
+        animationEventHandler.OnHitEnd -= HandleHitEnd;
+
         enemy.Agent.isStopped = false;
         enemy.Agent.updateRotation = true;
     }
 
     public override void LogicUpdate()
     {
+        enemy.Animator.SetFloat("Speed", 0f, 0.2f, Time.deltaTime);
+        enemy.Animator.SetFloat("Angle", 0f, 0.2f, Time.deltaTime);
+    }
 
+    private void HandleHitEnd()
+    {
+        if (enemy.SuspicionLevel >= enemy.Data.chaseThreshold)
+        {
+            stateMachine.ChangeState(enemy.ChaseState);
+            return;
+        }
+
+        if (enemy.SuspicionLevel >= enemy.Data.searchThreshold)
+        {
+            stateMachine.ChangeState(enemy.SearchState);
+            return;
+        }
+
+        stateMachine.ChangeState(enemy.PatrolState);
     }
 }
