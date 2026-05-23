218a8e3ae5a1fada7716270d69be7e22fa708ee6
ssb2ss <ssb2ss@naver.com>
2026-05-23 15:22:17 +0900

    ADD stat icons frame (not applied)

--- Assets/_Project/Scripts/02_Player/Item/ItemData/ConsumableItemData.cs
+++ Assets/_Project/Scripts/02_Player/Item/ItemData/ConsumableItemData.cs
@@ -1,5 +1,6 @@
 using FMODUnity;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Consumable", menuName = "Item Data/Consumable")]
@@ -16,8 +17,52 @@ public class ConsumableItemData : ItemData
     [Header("Sound Settings")]
     public EventReference consumeSound;
 
+    //public override string DescriptionString
+    //{
+    //    get
+    //    {
+    //        string baseDesc = base.DescriptionString;
+
+    //        if (effects == null || effects.Count == 0)
+    //        {
+    //            return baseDesc;
+    //        }
+
+    //        StringBuilder result = new StringBuilder(baseDesc);
+
+    //        result.AppendLine();
+    //        result.AppendLine();
+
+    //        // TOOD: use icons for stats instead of text
+    //        foreach (var effect in effects)
+    //        {
+    //            string iconTag = GetStatIconTag(effect.targetStat);
+    //            string sign = effect.totalAmount > 0 ? "+" : "";
+    //            string durationText = effect.duration > 0f ? $" ({effect.duration}s)" : "";
+
+    //            result.AppendLine($"{iconTag} {sign}{effect.totalAmount}{durationText}");
+    //        }
+
+    //        return result.ToString();
+    //    }
+    //}
+
     private void Reset()
     {
         useAnimationType = ItemUseAnimationType.ConsumeItem;
+    }
+
+    private string GetStatIconTag(ConditionType type)
+    {
+        const string assetName = "StatIcons";
+
+        return type switch
+        {
+            ConditionType.Health => $"<sprite=\"{assetName}\" name=\"Health\">",
+            ConditionType.Stamina => $"<sprite=\"{assetName}\" name=\"Stamina\">",
+            ConditionType.Satiety => $"<sprite=\"{assetName}\" name=\"Satiety\">",
+            ConditionType.Coldness => $"<sprite=\"{assetName}\" name=\"Coldness\">",
+            _ => ""
+        };
     }
 }
